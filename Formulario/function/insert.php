febff46fd3248e75318f4c1442f535f344d737c3
Bellezatabaskena <114634159+Bellezatabaskena@users.noreply.github.com>
2023-08-21 00:10:53 -0400

    20Agosto2023

--- Formulario/function/insert.php
+++ Formulario/function/insert.php
@@ -6,6 +6,9 @@ $nombre=$_POST['nombre'];
 $email=$_POST['email'];
 $Telefono=$_POST['Telefono'];
 
-print("<br> Nombre del Usuario: " . $nombre . "<br>email: " . $email . "<br>Tel: " . $Telefono);
+$query = "INSERT INTO usuario (nombre,email,tel) VALUES ('$nombre'.'$email','$Telefono')";
+
+$ejecutar = mysqli_query($connection, $query);
+//print("<br> Nombre del Usuario: " . $nombre . "<br>email: " . $email . "<br>Tel: " . $Telefono);
 
 ?>
--- Formulario/index.php
+++ Formulario/index.php
@@ -24,8 +24,9 @@
     <!-- place navbar here -->
   </header>
   <main>
-     <div class="container h-100" style="padding-top: 4rem;">
+     <div class="container my-3" style="padding-top: 4rem;">
          <div class="row justify-content-center align-item-center h-100">
+          <div class="col-6">
             <div class="card">
                 <div class="card-body">
                     <form action="./function/insert.php" method="post">
@@ -33,19 +34,56 @@
                           <label class="form-label">Nombre Completo</label>
                           <input name="nombre" type="text" class="form-control" >
                         </div>
-                        <div class="mb-3">
-                            <label class="form-label">Correo Electronico</label>
-                            <input name="email" type="email" class="form-control" >
-                          </div>
-                          <div class="mb-3">
-                            <label class="form-label">Telefono</label>
-                            <input name="Telefono" type="number" class="form-control" >
-                          </div>
-                        
-                        <button type="submit" class="btn btn-primary">Enviar</button>
-                      </form>
+                        <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Password</label>
+    <input type="email" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
                 </div>
               </div>
+              </div>
+              <div class="col-6">
+              <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">email</th>
+      <th scope="col">Telefono</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</div>
          </div>
 
      </div>
